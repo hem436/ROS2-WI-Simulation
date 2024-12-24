f33acfb1b9c7b9a3d9fcab293acd4a850f257097
hem436 <hemant436268s@gmail.com>
2024-12-24 11:13:15 +0530

    added robot to robot_controllers

---
+++ src/robot_controller/robot_controller/draw_circle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist, Vector3
+from turtlesim.msg import Pose
+
+class DrawCircle(Node):
+    
+    def __init__(self):
+        super().__init__('draw_circle')
+        self.cmd_vel_pub = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+        self.pose_sub = self.create_subscription(Pose, '/turtle1/pose', self.pose_callback, 10)
+        self.get_logger().info("Draw Circle Node Started")
+        # self.timer = self.create_timer(1.0, self.send_velocity_command)
+    
+    def send_velocity_command(self, x,z):
+        msg = Twist()
+        msg.linear.y = x
+        msg.angular.z = z
+        self.cmd_vel_pub.publish(msg)
+    
+    def pose_callback(self, msg: Pose):
+        if msg.x >9 or msg.x < 2 or msg.y > 9 or msg.y < 2:
+            self.send_velocity_command(1.0,1.0)
+            self.get_logger().info(f"Turning from X: {msg.x}, Y: {msg.y}")
+        else:
+            self.send_velocity_command(1.0,0.0)
+
+        
+def main():
+    rclpy.init()
+    node = DrawCircle()
+    node.get_logger().info("Hello World")
+    rclpy.spin(node)
+    rclpy.shutdown()
--- src/robot_controller/setup.py
+++ src/robot_controller/setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+from glob import glob
 
 package_name = 'robot_controller'
 
@@ -10,6 +11,11 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['launch/robot_description.launch.py']),
+        ('share/'+ package_name +"/urdf/standard/", glob('urdf/standard/*')),
+        ('share/'+ package_name+ "/urdf/sensors/", glob('urdf/sensors/*')),
+        ('share/'+ package_name+ "/urdf/lite/", glob('urdf/lite/*')),
+        ('share/'+ package_name+ "/meshes/", glob('meshes/*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +26,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "test_node = robot_controller.robot_node:main",
+            "draw_circle = robot_controller.draw_circle:main"
         ],
     },
 )
